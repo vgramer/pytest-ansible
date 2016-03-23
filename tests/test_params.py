693749c92c263b7b28df352e4298420304b1e955
James Laska <jlaska@ansible.com>
2016-03-23 10:08:19 -0400

    More debug/testing changes

--- tests/test_params.py
+++ tests/test_params.py
@@ -253,7 +253,22 @@ def test_param_override_with_marker(testdir):
             ansible_module.ping()
     """
     testdir.makepyfile(src)
-    result = testdir.runpytest(*['--tb', 'native', '--ansible-inventory', 'garbage,', '--ansible-host-pattern',
+    result = testdir.runpytest(*['-vvvvvs', '--tb', 'native', '--ansible-inventory', 'garbage,', '--ansible-host-pattern',
+                                 'garbage', '--ansible-connection', 'garbage'])
+    assert result.ret == EXIT_OK
+
+    # Mock assert the correct variables are set
+
+
+def test_param_override_with_marker_2(testdir):
+    src = """
+        import pytest
+        @pytest.mark.ansible(inventory='local,', connection='local', host_pattern='all')
+        def test_func(ansible_module):
+            ansible_module.ping()
+    """
+    testdir.makepyfile(src)
+    result = testdir.runpytest(*['-vvvvvs', '--tb', 'native', '--ansible-inventory', 'garbage,', '--ansible-host-pattern',
                                  'garbage', '--ansible-connection', 'garbage'])
     assert result.ret == EXIT_OK
 
